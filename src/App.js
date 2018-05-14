0b890fb26065ec92815465cf198849657d0339d4
Sai Phaninder Reddy Jonnala <sai.jonnala@gopassport.com>
2018-05-14 11:02:46 -0400

    material ui and icons and testing the material ui components.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import logo from './resources/logo.svg';
 import './resources/App.css';
+import Grid from 'material-ui/Grid';
+import Avatar from 'material-ui/Avatar';
+import Paper from 'material-ui/Paper';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    margin: 0,
+    padding: 10,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">#Golie</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className={styles.root}>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <Paper className={styles.paper}>
+                <Avatar>SJ</Avatar>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
       </div>
     );
   }
